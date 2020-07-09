08a85a413cdc03bf88dcd6de90ea90ae582aefc4
Joe Suárez <Joesuarez.developer@gmail.com>
2020-07-09 05:58:44 -0400

    Auth web delete

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Auth::routes();
 
 
 Route::get('/', 'HomeController@index')->name('home');
-Route::resource('usuarios', 'userController')->middleware('auth');
+Route::resource('usuarios', 'userController');
 
 Route::resource('sucursales', 'branchOfficeController');
 
